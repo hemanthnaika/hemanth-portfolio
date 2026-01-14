dc8993383d9ea51c2bf91432d286b81d597d60d2
hemanthnaika <hemanthnaika22@gmail.com>
2026-01-14 13:13:30 +0530

    update

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,12 @@
 import about from "./about1.png";
 import shapeAi from "./shapeAi.jpg";
 import microDegree from "./microDegree.png";
-import skillConnectMob from "./skillConnectMob.png";
+
 import skillConnectLap from "./skillConnect.png";
 import book from "./book.png";
 import pythonCer from "./PythonCer.png";
 import fullStack from "./fullStack.jpg";
-import shapeAiRec from "./shaPeAiRec.jpg";
+
 
 import pinnacle from "./pinnacle.jpg";
 import sygma from "./sygma.jpg";
@@ -21,12 +21,11 @@ export {
   about,
   shapeAi,
   microDegree,
-  skillConnectMob,
   skillConnectLap,
   book,
   pythonCer,
   fullStack,
-  shapeAiRec,
+
   pinnacle,
   sygma,
   omega,
